1a666023504a06457954579df89778d921a6927e
Suraj Prajapati <121746112+sp-suraj@users.noreply.github.com>
2026-05-20 01:05:09 +0530

    Add: design-hashmap - submission-1

---
+++ Data Structures & Algorithms/design-hashmap/submission-1.js
@@ -0,0 +1,50 @@
+class MyHashMap {
+    constructor() {
+        this.size = 1000;
+
+        this.buckets = Array.from({ length: this.size }, () => []);
+    }
+
+    hash(key) {
+        return key % this.size;
+    }
+
+    put(key, value) {
+        const index = this.hash(key);
+        const bucket = this.buckets[index];
+
+        for (let i = 0; i < bucket.length; i++) {
+            if (bucket[i][0] === key) {
+                bucket[i][1] = value;
+                return;
+            }
+        }
+
+        bucket.push([key, value]);
+    }
+
+    get(key) {
+        const index = this.hash(key);
+        const bucket = this.buckets[index];
+
+        for (let i = 0; i < bucket.length; i++) {
+            if (bucket[i][0] === key) {
+                return bucket[i][1];
+            }
+        }
+
+        return -1;
+    }
+
+    remove(key) {
+        const index = this.hash(key);
+        const bucket = this.buckets[index];
+
+        for (let i = 0; i < bucket.length; i++) {
+            if (bucket[i][0] === key) {
+                bucket.splice(i, 1);
+                return;
+            }
+        }
+    }
+}
