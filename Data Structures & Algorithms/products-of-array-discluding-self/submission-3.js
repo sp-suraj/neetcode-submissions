7658f90123a68db18b307f8d059d242f9215a828
Suraj Prajapati <121746112+sp-suraj@users.noreply.github.com>
2026-05-27 13:39:50 +0530

    Add: products-of-array-discluding-self - submission-3

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-3.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        let pre = Array.from({ length: nums.length }, () => 0),
+            post = Array.from({ length: nums.length }, () => 0),
+            result = Array.from({ length: nums.length }, () => 0);
+
+        for (let i = 0; i < nums.length; i++) {
+            if (i === 0) {
+                pre[i] = 1;
+            } else {
+                pre[i] = pre[i - 1] * nums[i - 1];
+            }
+        }
+
+        for (let i = nums.length - 1; i >= 0; i--) {
+            if (i === nums.length - 1) {
+                post[i] = 1;
+            } else {
+                post[i] = post[i + 1] * nums[i + 1];
+            }
+        }
+
+        for (let i = 0; i < nums.length; i++) {
+            result[i] = pre[i] * post[i];
+        }
+
+        return result;
+    }
+}
