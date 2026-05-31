0e4de298c6a278d2abc6e356a2a772b20ce2f236
Suraj Prajapati <121746112+sp-suraj@users.noreply.github.com>
2026-05-31 19:01:09 +0530

    Add: evaluate-reverse-polish-notation - submission-3

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-3.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string[]} tokens
+     * @return {number}
+     */
+    evalRPN(tokens) {
+        let stack = [];
+        for (let token of tokens) {
+            if (!["+", "-", "*", "/"].includes(token)) {
+                stack.push(token);
+            } else {
+                let n2 = stack.pop();
+                let n1 = stack.pop();
+                let resp;
+                if (token === "+") resp = parseInt(n1) + parseInt(n2);
+                if (token === "-") resp = parseInt(n1) - parseInt(n2);
+                if (token === "*") resp = parseInt(n1) * parseInt(n2);
+                if (token === "/")
+                    resp = Math.trunc(parseInt(n1) / parseInt(n2));
+                stack.push(resp);
+            }
+        }
+        return stack[0];
+    }
+}
